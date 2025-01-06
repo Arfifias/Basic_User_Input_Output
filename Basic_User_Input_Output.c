12790471a8d68f33c1d4cccc73585da5c08be3be
Aristeidis Stagkos <115788364+Arfifias@users.noreply.github.com>
2025-01-06 16:35:19 +0200

    Add files via upload

---
+++ Basic_User_Input_Output.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    char name [] = "Arfifias";
+    int age = 27;
+
+    printf("Please tell me your name\n");
+
+    printf("Okk %s....What about your age???\n",name);
+
+    printf("So you are %d years old hmmmm so we are peers\n",age);
+
+    printf("Thanks %s....it was nice meeting you", name);
+
+    
+
+    return 0;
+}
